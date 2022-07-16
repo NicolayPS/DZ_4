ccf86b5cc20324266be62e7b08c9f0dce77e3978
oksanaPC2 <77opod@gmail.com>
2022-07-16 15:40:34 +0300

    Добавьте файлы проекта.

---
+++ №2.2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("У вас 15 попыток");
+            int o = 0;
+            while (o < 15)
+            {
+
+
+                Console.Write("Введите размер A: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите размер B: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите размер C: ");
+                int c = Convert.ToInt32(Console.ReadLine());
+                while (a > 0 && b > 0 && c > 0 && c<(a+b))
+                {
+
+
+                    int d = 0;
+                    int e = 0;
+                    while (a >= c)
+                    {
+                        a -= c;
+                        d++;
+                    }
+                    while (b >= c)
+                    {
+                        b -= c;
+                        e++;
+                    }
+                    int count = 0;
+                    for (int i = 0; i < d; i++)
+                        count += e;
+                    Console.WriteLine("Число квадратов = " + count);
+
+                }
+                o++;
+            }
+            Console.WriteLine("Cпасибо!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ №2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("У вас 15 попыток");
+            int o = 0;
+            while (o < 15)
+            {
+                Console.WriteLine("Введите число:");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int s = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    s += 2 * i - 1;
+                }
+                Console.WriteLine(s);
+                o++;
+            }
+            Console.WriteLine("Cпасибо!");
+            Console.ReadKey();
+        }
+    }
+}
